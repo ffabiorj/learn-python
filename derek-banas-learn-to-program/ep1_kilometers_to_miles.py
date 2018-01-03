5804ae0d743af996c26e7dfa35f4371c4cb75dee
piratedv <thepiratedeveloper@gmail.com>
2018-01-03 22:49:50 +0800

    derek banas - kilometers to miles script

---
+++ derek-banas-learn-to-program/ep1_kilometers_to_miles.py
@@ -0,0 +1,7 @@
+# Problem : Receive miles and convert to kilometers
+# Kilometers = miles * 1.60934
+# Enter Miles 5 
+kilometers = input('Enter miles: ')
+kilometers = int(kilometers)
+# 5 Miles equals 
+print(f'{kilometers * 1.60934} miles')
