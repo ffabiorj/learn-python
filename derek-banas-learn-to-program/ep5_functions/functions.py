aa5ea2a729b49d6961e32f1eeb1582f4d286dfc7
piratedv <thepiratedeveloper@gmail.com>
2018-01-04 06:11:01 +0800

    derek banas - ep5 functions

--- derek-banas-learn-to-program/ep5_functions/functions.py
+++ derek-banas-learn-to-program/ep5_functions/functions.py
@@ -1,7 +1,20 @@
 def add_num(x, y):
     return x + y
 
-print(add_num(4,5))
 
-def assign_name():
-	pass
+glb_name = "Sally"
+
+
+def change_name():
+    global glb_name
+    glb_name = "Sammy"
+
+def get_sum(x, y):
+	sum = x + y
+
+print(get_sum(5,6))
+
+change_name()
+
+
+print(glb_name)
