efaae04a9c811bd5497bcf7f11b0efe86bff7104
piratedv <thepiratedeveloper@gmail.com>
2018-01-04 02:44:45 +0800

    derek banas - acronym generator

---
+++ derek-banas-learn-to-program/ep4_string_functions/acronym_generator.py
@@ -0,0 +1,14 @@
+# Ask for a string
+string = "Random Access Memory"
+ACR = ""
+# Convert the string to uppercase
+string = string.upper()
+
+# Convert the string into a list
+string = string.split()
+
+# Cycle through the list
+for i in string:
+    # Get the 1st letter of the word
+    print(i[0], end="")
+print()
