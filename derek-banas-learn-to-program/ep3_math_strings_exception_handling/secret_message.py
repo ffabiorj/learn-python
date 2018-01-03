1aa415262d0e9dcbb5118f8b66d8855f21907962
piratedv <thepiratedeveloper@gmail.com>
2018-01-04 02:44:11 +0800

    derek banas - secret message

---
+++ derek-banas-learn-to-program/ep3_math_strings_exception_handling/secret_message.py
@@ -0,0 +1,19 @@
+# Enter a string to hide in uppercase : HIDE
+# Secret Message : 35647890
+# Original Message: HIDE
+
+#norm_string = input("Enter a string to hide in uppercase")
+norm_string = "hide"
+secret_string = ""
+for char in norm_string:
+    secret_string += str(ord(char) - 23)
+
+print("Secret Message: ", secret_string)
+
+norm_string = ""
+
+for i in range(0, len(secret_string) -1, 2):
+    char_code = secret_string[i] + secret_string[i+1]
+    norm_string += chr(int(char_code) + 23)
+
+print("Original Message: ", norm_string)
