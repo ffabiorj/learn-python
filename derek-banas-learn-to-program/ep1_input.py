8ff029e9e0a744d8201904a8edcfed9667acffa8
piratedv <thepiratedeveloper@gmail.com>
2018-01-03 22:49:34 +0800

    derek banas basic inputs

---
+++ derek-banas-learn-to-program/ep1_input.py
@@ -0,0 +1,15 @@
+# Comment 
+'''
+Multi line comment
+'''
+
+## END
+
+# Ask the user to input their name and assign it to a
+# variable called 'name'
+
+name = input('What is your name: ')
+
+# Print out hello followed by the name they entered
+print('Hello ', name)
+
