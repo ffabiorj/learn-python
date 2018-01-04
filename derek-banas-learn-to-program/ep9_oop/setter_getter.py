8c69074c01b7ee8610c2649f5233918e7b8c6052
piratedv <thepiratedeveloper@gmail.com>
2018-01-05 05:39:22 +0800

    derek banas - setter and getter

---
+++ derek-banas-learn-to-program/ep9_oop/setter_getter.py
@@ -0,0 +1,48 @@
+class Square(object):
+    """docstring for Square"""
+
+    def __init__(self, height="0", width="0"):
+        self.height = height
+        self.width = width
+
+    @property
+    def height(self):
+        print("Retrieving the Height")
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if value.isdigit():
+            self.__height = value
+        else:
+            print("Please only enter digits for height.")
+
+    @property
+    def width(self):
+        print("Retrieving the Width")
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if value.isdigit():
+            self.__width = value
+        else:
+            print("Please only enter digits for width.")
+    
+    def getArea(self):
+        return int(self.__width * int(self.__height))
+
+def main():
+    aSquare = Square()
+    height = input("Enter height: ")
+    width = input("Enter width: ")
+
+    aSquare.height = height
+    aSquare.width = width
+
+    print("Height:", aSquare.height)
+    print("Width:", aSquare.width)
+
+    print("The area is:", aSquare.getArea())
+if __name__ == '__main__':
+    main()
