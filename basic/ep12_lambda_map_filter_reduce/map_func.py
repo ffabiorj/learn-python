2df569146d007e8e572e33c18ab262d2b3771451
piratedv <thepiratedeveloper@gmail.com>
2018-01-06 00:43:55 +0800

    map functions

---
+++ basic/ep12_lambda_map_filter_reduce/map_func.py
@@ -0,0 +1,11 @@
+onTo10 = range(1, 11)
+
+
+def dbl_num(num):
+    return num * 2
+
+print(list(map(dbl_num, onTo10)))
+print(list(map((lambda x: x * 3), onTo10)))
+
+aList = list(map((lambda x, y: x + y), [1, 2, 3], [1, 2, 3]))
+print(aList)
